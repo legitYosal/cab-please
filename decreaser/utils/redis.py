59b65e180ae5ffb420214f3916a31b2a61fd38e7
Yosef Salmalian <flaslnm@gmail.com>
2021-12-06 21:01:35 +0330

    change default surge ratings

--- decreaser/utils/redis.py
+++ decreaser/utils/redis.py
@@ -3,6 +3,6 @@ import config
 
 client = redis.Redis(host=config.REDIS_HOST, port=6379, db=0)
 
-def decrease_demand(demand_key: str):
+def decrease_demand(demand_key: str) -> None:
     if client.exists(demand_key):
         client.decr(demand_key)
